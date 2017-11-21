e3bbb908943dd596f1e4a03033afad69676fb8f5
546669204 <546669204@qq.com>
2017-11-21 15:35:41 +0800

    Fig "-" Missing

--- JsToBaseCss.js
+++ JsToBaseCss.js
@@ -1,58 +1,59 @@
-Array.prototype.unique = function(){
-	var result = [], hash = {};
-	for (var i = 0, elem; (elem = this[i]) != null; i++) {
-		if (!hash[elem]) {
-			result.push(elem);
-			hash[elem] = true;
-		}
-	}
-	return result;
+Array.prototype.unique = function() {
+    var result = [],
+        hash = {};
+    for (var i = 0, elem;
+        (elem = this[i]) != null; i++) {
+        if (!hash[elem]) {
+            result.push(elem);
+            hash[elem] = true;
+        }
+    }
+    return result;
 };
-$(document).ready(function(){
-	var start = new Date().getMilliseconds();
-	var ca = [];
-	var rule ={
-		'^bg(\\w*)$':["backgroundcolor:#{$$}"],
-		'^color(\\w*)$':["color:#{$$}"],
-		'^ml(\\w*)$':["marginleft:{$$}px"],
-		'^mr(\\w*)$':["marginright:{$$}px"],
-		'^mt(\\w*)$':["margintop:{$$}px"],
-		'^mb(\\w*)$':["marginbottom:{$$}px"],
-		'^padding(\\w*)$':["padding:{$$}px"],
-		'^pl(\\w*)$':["paddingleft:{$$}px"],
-		'^pr(\\w*)$':["paddingright:{$$}px"],
-		'^pt(\\w*)$':["paddingtop:{$$}px"],
-		'^pb(\\w*)$':["paddingbottom:{$$}px"],
-		'^fs(\\w*)$':["fontsize:{$$}px"],
-	};
-	$("body").append($("<style/>").attr("class","insetstyle"));
-	$("*").each(function(){
-		var c = $(this).attr("class");
-		if (c == undefined){
-			return true;
-		}
-		if(c.indexOf(" ")>=0){
-			ca = ca.concat(c.split(" "));
-		}else{
-			ca.push(c);
-		}
-	});
-	var classstr = ca.unique().join("\n");
-	for (var key in rule) {
-		if (rule.hasOwnProperty(key)) {
-			var element = rule[key];
-			var Re = new RegExp(key,"gm");
-			while ((rs = Re.exec(classstr)) != null)
-			{
-				var style = "." +
-					rs[0] +
-					"{" +
-					element[0].replace("{$$}",rs[1]) +
-					"}";
-				$(".insetstyle").append(style);
-			}
-		}
-	}
-　　var end = new Date().getMilliseconds();
-　　console.log(end-start);
+$(document).ready(function() {
+    var start = new Date().getMilliseconds();
+    var ca = [];
+    var rule = {
+        '^bg-(\\w*)$': ["background-color:#{$$}"],
+        '^color-(\\w*)$': ["color:#{$$}"],
+        '^ml-(\\w*)$': ["margin-left:{$$}px"],
+        '^mr-(\\w*)$': ["margin-right:{$$}px"],
+        '^mt-(\\w*)$': ["margin-top:{$$}px"],
+        '^mb-(\\w*)$': ["margin-bottom:{$$}px"],
+        '^padding-(\\w*)$': ["padding:{$$}px"],
+        '^pl-(\\w*)$': ["padding-left:{$$}px"],
+        '^pr-(\\w*)$': ["padding-right:{$$}px"],
+        '^pt-(\\w*)$': ["padding-top:{$$}px"],
+        '^pb-(\\w*)$': ["padding-bottom:{$$}px"],
+        '^fs-(\\w*)$': ["font-size:{$$}px"],
+    };
+    $("body").append($("<style/>").attr("class", "insetstyle"));
+    $("*").each(function() {
+        var c = $(this).attr("class");
+        if (c == undefined) {
+            return true;
+        }
+        if (c.indexOf(" ") >= 0) {
+            ca = ca.concat(c.split(" "));
+        } else {
+            ca.push(c);
+        }
+    });
+    var classstr = ca.unique().join("\n");
+    for (var key in rule) {
+        if (rule.hasOwnProperty(key)) {
+            var element = rule[key];
+            var Re = new RegExp(key, "gm");
+            while ((rs = Re.exec(classstr)) != null) {
+                var style = "." +
+                    rs[0] +
+                    "{" +
+                    element[0].replace("{$$}", rs[1]) +
+                    "}";
+                $(".insetstyle").append(style);
+            }
+        }
+    }　　
+    var end = new Date().getMilliseconds();　　
+    console.log(end - start);
 });
